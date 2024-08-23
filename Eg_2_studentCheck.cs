4802db70c34631dc562c86d012752cb718b501dd
RusteauN <rrusteau#gmail.com>
2024-08-23 20:50:25 +0400

    first c# files

---
+++ Eg_2_studentCheck.cs
@@ -0,0 +1,29 @@
+Console.Write("What is your First Name: ");
+string firstName = Console.ReadLine();
+Console.Write("What is your age: ");
+string ageText = Console.ReadLine();
+
+string formattedString;
+if (int.TryParse(ageText, out int age) == false)
+{
+    Console.WriteLine("You did not provide a valid age");
+    return;
+}
+
+if (firstName.ToLower() == "bob" || firstName.ToLower() == "sue")
+{
+    formattedString = $"Professor {firstName}";
+}
+else
+{
+    formattedString = firstName;
+}
+
+if (age < 21)
+{
+    Console.WriteLine($"Recoomend you wait {21 - age} years, {formattedString}");
+}
+else
+{
+    Console.WriteLine($"Welcome to class {formattedString}");
+}
